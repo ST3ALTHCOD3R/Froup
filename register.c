671f6fc71ecd480a47399f5a2fa5946558fe211b
Robert Fratila <robert.fratila@mail.mcgill.ca>
2016-04-06 00:20:49 -0400

    correctly parses POST input and stores the variables

--- register.c
+++ register.c
@@ -2,6 +2,56 @@
 #include<stdlib.h>
 #include<string.h>
 #define CONTENT_LENGTH getenv("CONTENT_LENGTH")
+
+void profile(char *tok){
+   char attribute[strlen(tok)];
+   int i;
+   for (i=0; i < strlen(tok); i++){ 
+      if(tok[i] != '='){
+         attribute[i] = tok[i];
+      }
+      else break;
+   }
+   attribute[i] = '\0';
+   i++;
+   int j = 0;
+   if(strcmp(attribute, "uName") == 0){//check username
+      for(; tok[i] != '\0'; i++){
+         attribute[j] = tok[i];
+         j++;
+      }
+      attribute[j] = '\0';
+      printf("Username: %s<br />", attribute);
+      //checkUnique(attribute);
+   }
+   else if(strcmp(attribute, "pWord") == 0){//password
+      for(; tok[i] != '\0'; i++){
+         attribute[j] = tok[i];
+         j++;
+      }
+      attribute[j] = '\0';
+      printf("Password: %s<br />", attribute);
+   } 
+   else if(strcmp(attribute, "fName") == 0){//full name
+      for(; tok[i] != '\0'; i++){
+         if(tok[i] == '+') attribute[j] = ' ';
+         else attribute[j] = tok[i];
+         j++;
+      }
+      attribute[j] = '\0';
+      printf("Name: %s<br />", attribute);
+   }
+   else if(strcmp(attribute, "uJob") == 0){//job description
+      for(; tok[i] != '\0'; i++){
+         if(tok[i] == '+') attribute[j] = ' ';
+         else attribute[j] = tok[i];
+         j++;
+      }
+      attribute[j] = '\0';
+      printf("Description: %s<br />", attribute);
+   }
+}
+
 int main(void){
    int n;
    char *token = NULL;
@@ -18,7 +68,7 @@ int main(void){
             printf("str length: %d and string is: %s.<br />",n, manString);
             token = strtok(manString, "&");
             while(token != NULL){
-               printf("token: %s<br />", token);
+               profile(token);
                token = strtok(NULL, "&");
             }
          }
