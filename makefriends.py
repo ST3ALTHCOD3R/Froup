d7013f138dbb23b33f11664be48b6060b5fe76e2
Robert Fratila <robert.fratila@mail.mcgill.ca>
2016-04-08 17:50:46 -0400

    finds user and their full name

--- makefriends.py
+++ makefriends.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python
 import sys
 import os
 import stat
 import cgi
 
 def readFile():
-	print "Content-type:text/html\n\n"
+	print "Content-Type: text/html\n\n"
 	print "<html><head><title>Make Friends</title></head>"
 	print "<body>"
 	try:
 		input = open("users.txt","r")
 		read = input.readlines()
 		inputTuple = []
+		i=0
 		for line in read:
 			singleLine = line.rstrip()
-			print singleLine
+			if(i%4 == 0):
+				print "Username: ",singleLine,"<br />"
+			if(i%4 == 2):
+				print "Full Name: ",singleLine,"<br />"
+			i+=1
 			inputTuple.append(singleLine)
 	except IOError, (errno, strerror):
 		print "I/O error(%s): %s"%(errno, strerror)
