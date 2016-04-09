2cb0b53d7457f82b213d42bcfd8fcbf611222c16
Robert Fratila <robert.fratila@mail.mcgill.ca>
2016-04-08 22:08:35 -0400

    friend addition works both ways, crashes if user chooses themselves

--- newfriends.py
+++ newfriends.py
@@ -3,19 +3,33 @@ import cgi
 
 def addFriends():
 	currentUser = "Michael"
-	form = cgi.FieldStorage() #put recieved data into dictionary
-	print "Content-type:text/html\n\n"
-	print "<html><body>"
-	for key in form:
-		print key,"<br />"
 	input = open("friends.txt","r")
-	read = input.readlines()
-	dict = {}#to store users and their friends
-	for line in read:
-		parse = line.split(" ")
-		user = parse[0]
-		parse.remove(parse[0])
-		dict[user] = parse
-	print dict,"<br />"
+        read = input.readlines()
+        dict = {}#to store users and their friends
+	print "Content-type:text/html\n\n"
+        print "<html><body>"
+        for line in read:
+		line = line.replace("\n", "")
+                parse = line.split(" ")
+                user = parse[0]
+                parse.remove(parse[0])
+                dict[user] = parse
+
+	form = cgi.FieldStorage() #put recieved data into dictionary
+	friendsChosen = []#stores friends chosen in a list
+	for tok in form:
+		tok = tok.replace(" ","")
+		friendsChosen.append(tok)
+	print "START",dict,"<br />"
+	if currentUser in dict: #if user already has friends in database
+		for user in friendsChosen:
+			if user not in dict[currentUser]: #if friend is unique
+				dict[currentUser].append(user)
+				#dict[user].append(currentUser) #friendship goes both ways
+	else: #user does not have any friends yet
+		dict[currentUser] = friendsChosen #add all chosen users to be friends
+		for friend in friendsChosen: #user is a friend to the ones he/she chose
+			dict[friend].append(currentUser)
+	print "FINISH",dict
 	print "</body></html>"
 addFriends()	
