0f9d3172f74890c0e2e19200b4ff6e520703128b
Robert Fratila <robert.fratila@mail.mcgill.ca>
2016-04-08 20:48:39 -0400

    stores all users and their corresponding friends in a dictionary

--- newfriends.py
+++ newfriends.py
@@ -2,10 +2,20 @@
 import cgi
 
 def addFriends():
+	currentUser = "Michael"
 	form = cgi.FieldStorage() #put recieved data into dictionary
 	print "Content-type:text/html\n\n"
 	print "<html><body>"
 	for key in form:
 		print key,"<br />"
+	input = open("friends.txt","r")
+	read = input.readlines()
+	dict = {}#to store users and their friends
+	for line in read:
+		parse = line.split(" ")
+		user = parse[0]
+		parse.remove(parse[0])
+		dict[user] = parse
+	print dict,"<br />"
 	print "</body></html>"
 addFriends()	
